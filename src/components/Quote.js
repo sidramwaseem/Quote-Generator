f87ceaa5fde37e92b558bd76242c5e2f62264bc8
sidramwaseem <sidraw24@gmail.com>
2022-05-08 23:05:00 +0500

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Quote(){
   return(
-    <div className="quote-container"></div>
+    <main className="quote-container">
+    <p className="quote--text">Just as characteristic, perhaps, is the intellectual interdependence created through the development of the modern media of communication: post, telegraph, telephone, and popular press.</p>
+    <div className="quote--credits">
+    <h3 className="quote--author">Christian Lous Lange</h3>
+    <p className="quote--genre">communication</p>
+    </div>
+    </main>
   )
 }
