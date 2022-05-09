4930a22236e1aece2541b4411e1027345cbf9fe1
sidramwaseem <sidraw24@gmail.com>
2022-05-09 23:32:39 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import arrow from "../images/arrow.png";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <header className="header">
       <h1>Random</h1>
-      <figure>
+      <figure onClick={props.handleClick}>
         <img src={arrow} alt="loading-arrow" className="arrow-img" />
       </figure>
     </header>
