c7a41d13c445591700f4c2836cdb943bb88295c7
sidramwaseem <sidraw24@gmail.com>
2022-05-09 23:32:50 +0500

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import  straightArrow from "../images/straightarrow.png";
 
-export default function Quote(){
-  return(
+export default function Quote(props) {
+  return (
     <main className="quote-container">
-    <p className="quote--text">Just as characteristic, perhaps, is the intellectual interdependence created through the development of the modern media of communication: post, telegraph, telephone, and popular press.</p>
-    <div className="quote--credits">
-    <h3 className="quote--author">Christian Lous Lange</h3>
-    <p className="quote--genre">communication</p>
-    </div>
+      <p className="quote--text">
+      {props.text}
+      </p>
+      <div className="quote--credits-container">
+        <div className="quote--credits">
+          <h3 className="quote--author">{props.author}</h3>
+          <p className="quote--genre">{props.genre}</p>
+        </div>
+        <figure>
+        <img src={straightArrow} alt="next-arrow" className="straight-arrow"/>
+        </figure>
+      </div>
     </main>
-  )
+  );
 }
