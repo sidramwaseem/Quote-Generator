2fd964e0a50b4cb8e3709dd9de587c63769ec3a0
sidramwaseem <sidraw24@gmail.com>
2022-05-09 23:32:29 +0500

    fetch API

--- src/App.js
+++ src/App.js
@@ -4,10 +4,49 @@ import Quote from "./components/Quote.js";
 import Footer from "./components/Footer.js";
 
 export default function App() {
+  const [quote, setQuote] = React.useState({
+    text: "Just as characteristic, perhaps, is the intellectual interdependence created through the development of the modern media of communication: post, telegraph, telephone, and popular press.",
+    author: "Christian Lous Lange",
+    genre: "communication",
+  });
+
+  React.useEffect(() => {
+    async function getQuote() {
+      const res = await fetch(
+        "https://quote-garden.herokuapp.com/api/v3/quotes/random"
+      );
+      const data = await res.json();
+      const { quoteText, quoteAuthor, quoteGenre } = data.data[0];
+      setQuote(() => {
+        return {
+          text: quoteText,
+          author: quoteAuthor,
+          genre: quoteGenre,
+        };
+      });
+    }
+    getQuote();
+  }, []);
+
+  async function toggleChange() {
+    const res = await fetch(
+      "https://quote-garden.herokuapp.com/api/v3/quotes/random"
+    );
+    const data = await res.json();
+    const { quoteText, quoteAuthor, quoteGenre } = data.data[0];
+    setQuote(() => {
+      return {
+        text: quoteText,
+        author: quoteAuthor,
+        genre: quoteGenre,
+      };
+    });
+  }
+
   return (
     <div className="container">
-      <Header />
-      <Quote />
+      <Header handleClick={toggleChange} />
+      <Quote text={quote.text} author={quote.author} genre={quote.genre} />
       <Footer />
     </div>
   );
