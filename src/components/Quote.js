0b36b740303658752043128570c8291712b09f88
sidramwaseem <sidraw24@gmail.com>
2022-05-14 14:11:42 +0500

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,19 +1,24 @@
 import React from "react";
-import  straightArrow from "../images/straightarrow.png";
+import straightArrow from "../images/straightarrow.png";
 
 export default function Quote(props) {
+  // const { randomQuote, authorQuotes, genreQuotes, getQuotes, searchQuotes } = require("quotegarden");
+
+
   return (
     <main className="quote-container">
-      <p className="quote--text">
-      {props.text}
-      </p>
+      <p className="quote--text">{props.text}</p>
       <div className="quote--credits-container">
         <div className="quote--credits">
           <h3 className="quote--author">{props.author}</h3>
           <p className="quote--genre">{props.genre}</p>
         </div>
         <figure>
-        <img src={straightArrow} alt="next-arrow" className="straight-arrow"/>
+          <img
+            src={straightArrow}
+            alt="next-arrow"
+            className="straight-arrow"
+          />
         </figure>
       </div>
     </main>
